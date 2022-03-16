40e29a8cbc9b4a96051a9fc9d3a8a38983b6b5f1
LuanSouzaProg <luandesouza2001@rede.ulbra.br>
2022-03-16 15:13:24 -0300

    Flutterando

---
+++ lib/algoritmo/funcoes.dart
@@ -0,0 +1,12 @@
+void main(){
+  minhaFuncao("Luan");
+  somar(2, 4);
+}
+
+minhaFuncao(String text){
+  print(text);
+}
+
+somar(int n1, int n2){
+  print(n1 + n2);
+}
---
+++ lib/algoritmo/refatoracao_imc.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+void main(){
+
+  final String name = getName();
+
+  //Calculadora IMC
+
+  double peso = getPeso();
+  double altura = getAltura();
+
+  double resultado = peso / (altura * altura);
+
+  //print("O seu IMC ficou : $resultado");
+
+  print("Nome : $name");
+  if(resultado > 25){
+    print("Você está gordo");
+  }else if(resultado < 18){
+    print("vVocê está magro");
+  }else {
+    print("Peso ideal");
+  }
+}
+
+String getName(){
+  print("digite seu nome : ");
+  final String? name = stdin.readLineSync();
+
+   if(name == null){
+     return "Anônimo";
+   }else{
+     return name;
+   }
+}
+
+double getPeso(){
+  print("digite seu peso : ");
+  final String? stringPeso = stdin.readLineSync();
+
+  if(stringPeso == null){
+    return 0.0;
+  }else{
+    return double.parse(stringPeso);
+  }
+}
+
+double getAltura(){
+  print("digite sua altura : ");
+  final String? stringAltura = stdin.readLineSync();
+
+  if(stringAltura == null) {
+    return 0.0;
+  }else{
+    return double.parse(stringAltura);
+  }
+}
